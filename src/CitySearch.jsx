ee534fbfa05d6c7e208bb2b087f862b2f80886f8
Hana Pourmohebbi <h.pourmohebbi@gmail.com>
2024-09-15 14:00:07 +0430

    define a new useState matchedCity  instead of cityfilter

--- src/CitySearch.jsx
+++ src/CitySearch.jsx
@@ -3,26 +3,9 @@ import cities from './cities.json'
 import styles from './Citysearch.module.css'
 
 function CitySearch() {
-  const [inputValue,setInputValue]=useState('');
-  const [cityFilter , setCityFilter]=useState([]);
-  // function inputChangeHandler (c){
-  //   const value=c.target.value;
-  //   setInputValue(value);
-  //   const match=cities.find((city)=>city.toLocaleLowerCase()===value.toLocaleLowerCase());
-  //   if(match){
-  //     setCityGuess(match);
-  //   }else{
-  //     setCityGuess('');
-  //   }
-  //     }
-  useEffect(()=>{
-    if(inputValue.length>0){
-      const filtered=cities.filter((city)=>
-      city.toLocaleLowerCase().includes(inputValue.toLocaleLowerCase()));
-      setCityFilter(filtered);
-    }else{setCityFilter([])}
-  },[inputValue,cities]);
-
+  const [inputValue,setInputValue]=useState("");
+  const [matchedCity, setMatchedCity]=useState("");
+ 
 
   return (
     <div className={styles.container}>
